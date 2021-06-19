d9c1975958248c6f87470f92d397e60cd105b480
Viktoria <doduchenko29@gmail.com>
2021-06-20 01:57:15 +0300

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+const mult = (a) => (b) => a * b;
+
+export const twice = mult(2);
+const result = twice(5)
+
+export const triple = mult(3);
+const result2 = triple(5)
+
+console.log(result)
+console.log(result2)
